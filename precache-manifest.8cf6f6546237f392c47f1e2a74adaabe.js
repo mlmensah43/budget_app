e898b25b606b77b622a406d495db7541fbf9e0a2
Mariah Mensah <mariahm207@gmail.com>
2019-11-12 23:36:55 -0600

    Updates

---
+++ precache-manifest.8cf6f6546237f392c47f1e2a74adaabe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99d63372002f3edf6613e5c0afb0635a",
+    "url": "/budget_app/index.html"
+  },
+  {
+    "revision": "ce2b08ea37a20800a949",
+    "url": "/budget_app/static/css/main.3183ecca.chunk.css"
+  },
+  {
+    "revision": "84e5aee50e1e500c9a51",
+    "url": "/budget_app/static/js/2.01a8ccfe.chunk.js"
+  },
+  {
+    "revision": "ce2b08ea37a20800a949",
+    "url": "/budget_app/static/js/main.11fe1de2.chunk.js"
+  },
+  {
+    "revision": "41c6e812705d28b48319",
+    "url": "/budget_app/static/js/runtime-main.66217b42.js"
+  },
+  {
+    "revision": "acc2904ed59edc3d72be9f29774b519c",
+    "url": "/budget_app/static/media/cloudyknoxville.acc2904e.png"
+  },
+  {
+    "revision": "cad67b2b062f2e8c94e9b19b8d2a3d73",
+    "url": "/budget_app/static/media/logo.cad67b2b.png"
+  },
+  {
+    "revision": "76e271f3715bedcdbabd396059562a4d",
+    "url": "/budget_app/static/media/piggy.76e271f3.jpeg"
+  }
+]);
